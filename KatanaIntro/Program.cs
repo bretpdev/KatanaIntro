a1130593d82e33e2b02ad07e7a3f513201bcad48
Bret Pehrson <bpehrson@utahsbr.edu>
2018-06-29 15:33:37 -0600

    Initial commit

---
+++ KatanaIntro/Program.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KatanaIntro
+{
+    using System.IO;
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uri = "http://localhost:8080";
+
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+                Console.WriteLine("Stopping!");
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.UseWelcomePage();
+
+            //app.Run(ctx =>
+            //{
+            //    return ctx.Response.WriteAsync("Hello World");
+            //});
+
+            //app.Use<HelloWorldComponent>();
+
+            //app.Use(async (environment, next) =>
+            //{
+            //    foreach (var pair in environment.Environment)
+            //    {
+            //        Console.WriteLine("{0}:{1}", pair.Key, pair.Value);
+            //    }
+
+            //    await next();
+            //});
+
+            app.Use(async (environment, next) =>
+            {
+                Console.WriteLine("Requesting : " + environment.Request.Path);
+
+                await next();
+
+                Console.WriteLine("Response : " + environment.Response.StatusCode);
+            });
+        }
+    }
+
+    public class HelloWorldComponent
+    {
+        AppFunc _next;
+        public HelloWorldComponent(AppFunc next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(IDictionary<string, object> environment)
+        {
+            //await _next(environment);
+
+            var response = environment["owin.ResponseBody"] as Stream;
+
+            using (var writer = new StreamWriter(response))
+            {
+                return writer.WriteAsync("Hello!!");
+            }
+        }
+
+        //public async Task Invoke(IDictionary<string, object> environment)
+        //{
+        //    await _next(environment);
+        //}
+    }
+}
